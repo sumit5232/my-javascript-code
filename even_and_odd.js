d8a45098a039af4bb11c16210a3d43360b4abdc8
sumit5232 <sumityadav5232@gmail.com>
2022-12-05 12:32:13 +0530

    this is my first commit

---
+++ even_and_odd.js
@@ -0,0 +1,15 @@
+var i;
+function even(i)
+{
+    
+    for( i=1;i<=100;i++)
+    {
+        if(i%2==0)
+        {
+            console.log(i);
+        }
+        else{
+        }
+    }
+}
+even(i);
---
+++ sum1.js
@@ -0,0 +1,7 @@
+var a=3;
+var b=4;
+function add(a,b) {
+	sum=a+b;
+	console.log(sum);
+}
+add(5,7);
